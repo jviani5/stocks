b97380d9920ef152a614c417eb5e11d7f41e7b06
Jared Viani <jaredviani5@gmail.com>
2023-04-02 18:35:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import requests
 
 # ticker search feature in sidebar
 st.sidebar.subheader("""Stock Search Web App""")
-selected_stock = st.sidebar.text_input("Why...", "Enter Ticker")
+st.sidebar.subheader("""Why""")
+selected_stock = st.sidebar.text_input("", "AAPL")
 
 # Initialize an empty DataFrame with columns "ticker", "price", and "5yr %"
 df = pd.DataFrame(columns=["ticker", "price", "5yr %"])
